79778c6210eb1684bf25b353f10a430e1e205b34
seyyitatim <seyyitatim@gmail.com>
2022-07-06 18:16:05 +0300

    Product ile basit bir geliştirilme yapıldı

---
+++ ECommerceBackEnd/ECommerce/Infrastructure/ECommerce.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Application.Abstractions;
+using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts() => new()
+        {
+            new() {Id = Guid.NewGuid(), Name="Product 1", Price = 100, Stock = 10, CreateDate = DateTime.Now},
+            new() {Id = Guid.NewGuid(), Name="Product 2", Price = 100, Stock = 10, CreateDate = DateTime.Now},
+            new() {Id = Guid.NewGuid(), Name="Product 3", Price = 100, Stock = 10, CreateDate = DateTime.Now},
+            new() {Id = Guid.NewGuid(), Name="Product 4", Price = 100, Stock = 10, CreateDate = DateTime.Now},
+            new() {Id = Guid.NewGuid(), Name="Product 5", Price = 100, Stock = 10, CreateDate = DateTime.Now}
+        };
+    }
+}
