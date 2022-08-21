13c3c5b6f1312181b7fc1379cd8795cd3fb5c911
seyyitatim <seyyitatim@gmail.com>
2022-08-21 22:48:01 +0300

    fluent validation

---
+++ ECommerceBackEnd/ECommerce/Core/ECommerce.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Application.ViewModels;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Products>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().NotNull().WithMessage("Lütfen ürün adını boş geçmeyiniz.");
+        }
+    }
+}
---
+++ ECommerceBackEnd/ECommerce/Core/ECommerce.Application/ViewModels/VM_Create_Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.ViewModels
+{
+    public class VM_Create_Products
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+    }
+}
