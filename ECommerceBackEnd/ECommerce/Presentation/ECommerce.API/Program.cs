de7129e3636ee44e89bc3bbacfa2c7ab4ca1b04d
seyyitatim <seyyitatim@gmail.com>
2022-08-22 19:53:16 +0300

    validator eklendi

--- ECommerceBackEnd/ECommerce/Presentation/ECommerce.API/Program.cs
+++ ECommerceBackEnd/ECommerce/Presentation/ECommerce.API/Program.cs
@@ -1,4 +1,7 @@
-﻿using ECommerce.Persistence;
+﻿using ECommerce.Application.Validators.Products;
+using ECommerce.Infrastructure.Filters;
+using ECommerce.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,9 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddPersistenceServices();
 // CORS ayarı
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
-  policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
+    policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
 
-builder.Services.AddControllers();
+// eklediğimiz validator kurallarını uygulamamıza tanıttığımız kısım. RegisterValidatorsFromAssemblyContaining methodu ile verdiğimiz bir tane class ile o classın bulunduğu assemblydeki tüm validatorları bularak uygulamamıza tanıtmamıza yarıyor.
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+    .ConfigureApiBehaviorOptions(options=>options.SuppressModelStateInvalidFilter = true); // validator kontrolünün yazacağımız customer filter ile yapılacağınından dolayı default filterı kaldırıyoruz.
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
