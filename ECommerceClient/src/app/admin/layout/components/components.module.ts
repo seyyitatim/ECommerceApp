4684de2a77df03d9b44e7e6dcb42e213966e6a28
seyyitatim <seyyitatim@gmail.com>
2023-03-04 13:28:27 +0300

    router yapısı kuruldu

--- ECommerceClient/src/app/admin/layout/components/components.module.ts
+++ ECommerceClient/src/app/admin/layout/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, SidebarComponent, FooterComponent],
 })
 export class ComponentsModule {}
