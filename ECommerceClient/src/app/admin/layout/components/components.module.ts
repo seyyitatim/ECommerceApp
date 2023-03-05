0287d9639da0a284db7f96d325f37a9a47dbbf1e
seyyitatim <seyyitatim@gmail.com>
2023-03-05 15:56:44 +0300

    temel admin template i eklendi

--- ECommerceClient/src/app/admin/layout/components/components.module.ts
+++ ECommerceClient/src/app/admin/layout/components/components.module.ts
@@ -4,10 +4,10 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
-
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent, FooterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatListModule],
   exports: [HeaderComponent, SidebarComponent, FooterComponent],
 })
 export class ComponentsModule {}
