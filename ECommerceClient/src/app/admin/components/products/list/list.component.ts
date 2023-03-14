1f2dcdad132895176fe9dea17d154b4ecd55cf33
seyyitatim <seyyitatim@gmail.com>
2023-03-14 23:36:29 +0300

    listemele özelliği eklendi

--- ECommerceClient/src/app/admin/components/products/list/list.component.ts
+++ ECommerceClient/src/app/admin/components/products/list/list.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent } from 'src/app/base/base.component';
+import { ListProduct } from 'src/app/contracts/list_product';
+import { ProductService } from 'src/app/services/common/product.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
-export class ListComponent {
+export class ListComponent extends BaseComponent implements OnInit {
+  products: ListProduct[];
+  pageCount: number;
+  currencyPage: number = 0;
+  constructor(
+    spinner: NgxSpinnerService,
+    private productService: ProductService
+  ) {
+    super(spinner);
+  }
 
+  async ngOnInit() {
+    await this.getProducts();
+  }
+
+  async getProducts(page: number = 0) {
+    const data = await this.productService.list(
+      page,
+      5,
+      () => {},
+      () => {}
+    );
+    this.currencyPage = page;
+    this.products = data.products;
+    this.pageCount = data.pageCount;
+  }
+  async after() {
+    if (this.currencyPage + 1 >= this.pageCount) return;
+    this.currencyPage += 1;
+    await this.getProducts(this.currencyPage);
+  }
+  async before() {
+    if (this.currencyPage - 1 >= 0) return;
+    this.currencyPage -= 1;
+    await this.getProducts(this.currencyPage);
+  }
 }
